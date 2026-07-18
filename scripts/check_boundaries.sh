44e86f5c7e8899947bb99d12b8e3aa697cdd6309
d954mas <d954mas@gmail.com>
2026-07-18 17:19:31 +0500

    refactor(journal): split wire codecs

--- scripts/check_boundaries.sh
+++ scripts/check_boundaries.sh
@@ -446,7 +446,7 @@ tp_encode_internal      tp_op_encode|tp_txn_encode|tp_txn_apply
 tp_fs_internal          tp_fs_io|tp_fs_win32|tp_fs_posix|tp_export_defold|tp_export_json_neotolis|tp_export_png|tp_identity|tp_image|tp_journal_io|tp_pack_read|tp_project_parse|tp_project_save|tp_project_lease|tp_recovery|tp_recovery_backend_win32|tp_recovery_scan|tp_recovery_store|tp_scan
 tp_pack_constraints_internal tp_pack_constraints|tp_op_validate_atlas|tp_op_validate_source_sprite|tp_pack|tp_project_identity|tp_project_parse|tp_validate_target_settings
 tp_history_codec_internal tp_history|tp_history_codec|tp_model_journal|tp_txn_apply
-tp_journal_internal     tp_journal|tp_journal_io|tp_history|tp_model|tp_model_journal|tp_txn_apply|tp_recovery_backend_posix|tp_recovery_backend_win32|tp_recovery_claim|tp_recovery_scan|tp_recovery_store
+tp_journal_internal     tp_journal|tp_journal_io|tp_journal_wire|tp_history|tp_model|tp_model_journal|tp_txn_apply|tp_recovery_backend_posix|tp_recovery_backend_win32|tp_recovery_claim|tp_recovery_scan|tp_recovery_store
 tp_json_internal        tp_json_internal|tp_project_parse|tp_txn_parse
 tp_utf8_internal        tp_utf8|tp_fs_io|tp_image|tp_json_internal|tp_op_validate|tp_project_identity|tp_source_path_text
 tp_idset_internal       tp_idset|tp_txn_idset|tp_journal|tp_model_journal
